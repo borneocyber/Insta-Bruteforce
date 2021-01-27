de0879e23f52b760a9259162b261b4bccbf8ec3e
borneocyber <78080890+borneocyber@users.noreply.github.com>
2021-01-27 20:05:30 +0800

    Add files via upload

---
+++ lib/__init__.py
@@ -0,0 +1,2 @@
+# update: 01/27/2021
+# Author: Borneo Cyber
---
+++ lib/proxy_list.py
@@ -0,0 +1,18 @@
+# update: 01/27/2021
+# Author: Borneo Cyber
+# Description: A list that will manage proxies 
+
+
+class ProxyList(object):
+
+    def __init__(self):
+        self.list = []
+
+    def __contains__(self, proxy):
+        for _proxy in self.list:
+            if _proxy.ip == proxy['ip'] and _proxy.port == proxy['port']:
+                return True 
+        return False 
+
+    def append(self, proxy):
+        self.list.append(proxy) 
